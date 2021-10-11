48beceec296ccd643a1e218e277e7e9b05059adb
Chaya Phillips <cphillip@g.jct.ac.il>
2021-10-11 18:08:11 +0300

    Initail code + Welcome function

---
+++ Targil0/Program8636.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8636();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6776();
+        private static void Welcome8636()
+        {
+            Console.Write("Enter your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine($"{ name}, welcome to my first console application");
+        }
+    }
+}
