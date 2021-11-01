80b6857d9e0a64d4038c8b2ba5c8b472d7955964
Chaya Phillips <cphillip@g.jct.ac.il>
2021-11-01 11:48:41 +0200

    update

--- DAL/Location.cs
+++ DAL/Location.cs
@@ -65,12 +65,12 @@ namespace IDAL
             /// <param name="lat2">the latitude of the second location</param>
             /// <param name="lng2">the longitude of the second location</param>
             /// <returns>the distance in km</returns>
-            public static double GetDistance(double longitude, double latitude, double otherLongitude, double otherLatitude)
+            public static double GetDistance(double lng1, double lat1, double lng2, double lat2)
             {
-                var d1 = latitude * (Math.PI / 180.0);
-                var num1 = longitude * (Math.PI / 180.0);
-                var d2 = otherLatitude * (Math.PI / 180.0);
-                var num2 = otherLongitude * (Math.PI / 180.0) - num1;
+                var d1 = lat1 * (Math.PI / 180.0);
+                var num1 = lng1 * (Math.PI / 180.0);
+                var d2 = lat2 * (Math.PI / 180.0);
+                var num2 = lng2 * (Math.PI / 180.0) - num1;
                 var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
                 return (6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)))) / 1000;
             }
