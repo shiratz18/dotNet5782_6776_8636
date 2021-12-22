acc7a3d78c76336eadb13c2fdf0927d0ff87224d
shiratz18 <stzadka@g.jct.ac.il>
2021-12-22 15:52:24 +0200

    changes

--- PL/DroneListWindow.xaml.cs
+++ PL/DroneListWindow.xaml.cs
@@ -155,7 +155,6 @@ namespace PL
         /// <param name="e"></param>
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-            allowClosing = true;
             this.Close();
         }
 
@@ -220,56 +219,6 @@ namespace PL
             statusLabel.Content = "Status";
         }
 
-        //dont allow closig with x button
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-
-            HwndSource hwndSource = PresentationSource.FromVisual(this) as HwndSource;
-
-            if (hwndSource != null)
-            {
-                hwndSource.AddHook(HwndSourceHook);
-            }
-
-        }
-
-        private bool allowClosing = false;
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-        [DllImport("user32.dll")]
-        private static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
-
-        private const uint MF_BYCOMMAND = 0x00000000;
-        private const uint MF_GRAYED = 0x00000001;
-
-        private const uint SC_CLOSE = 0xF060;
-
-        private const int WM_SHOWWINDOW = 0x00000018;
-        private const int WM_CLOSE = 0x10;
-
-        private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            switch (msg)
-            {
-                case WM_SHOWWINDOW:
-                    {
-                        IntPtr hMenu = GetSystemMenu(hwnd, false);
-                        if (hMenu != IntPtr.Zero)
-                        {
-                            EnableMenuItem(hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED);
-                        }
-                    }
-                    break;
-                case WM_CLOSE:
-                    if (!allowClosing)
-                    {
-                        handled = true;
-                    }
-                    break;
-            }
-            return IntPtr.Zero;
-        }
+        
     }
 }
