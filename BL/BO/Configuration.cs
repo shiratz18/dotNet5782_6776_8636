607c53d74887410b9de230b8c2da543f506bddd8
shiratz18 <stzadka@g.jct.ac.il>
2022-01-05 00:28:57 +0200

    forgotPasswordWindow

---
+++ BL/BO/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+
+namespace BO
+{
+    class Configuration
+    {
+        static public string question = "What is your mother's maiden name?";
+        static public string answer = "xxxx";
+
+    }
+}
